c75804a80d56412742cd33fe56821a14b94c22c6
MURPHY Paul <pmurphy@murex.com>
2016-02-18 12:09:37 +0000

    first commit

---
+++ src/Problem1.scala
@@ -0,0 +1,16 @@
+package com.paul.problems
+
+object Problem10To20 {
+  
+  
+  def main(args: Array[String]): Unit = {
+    
+  }
+  
+  // Tenth
+  def modifiedRunLengthEncoding[A](list:List[A]) : List[Tuple2[A, Int]] = {
+     return Nil
+  }
+  
+  
+}
